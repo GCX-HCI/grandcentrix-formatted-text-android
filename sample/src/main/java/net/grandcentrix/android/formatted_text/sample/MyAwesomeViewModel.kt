e00a13b47b457607a8c6a073ec1135c51fe9d753
Fabian Kotschenreuther <fabian.kotschenreuther@grandcentrix.net>
2021-02-24 13:08:15 +0100

    Refactoring binding adapter

--- sample/src/main/java/net/grandcentrix/android/formatted_text/sample/MyAwesomeViewModel.kt
+++ sample/src/main/java/net/grandcentrix/android/formatted_text/sample/MyAwesomeViewModel.kt
@@ -1,6 +1,9 @@
 package net.grandcentrix.android.formatted_text.sample
 
-import net.grandcentrix.android.formatted_text.*
+import net.grandcentrix.android.formatted_text.FormattedText
+import net.grandcentrix.android.formatted_text.NullFallbackText
+import net.grandcentrix.android.formatted_text.formatWithFallback
+import net.grandcentrix.android.formatted_text.textIfNull
 
 /**
  * A simple view model witch demonstrate the [FormattedText] and [NullFallbackText].
@@ -13,6 +16,6 @@ class MyAwesomeViewModel {
     val formattedNullNumberWithFallback =
         nullInt.formatWithFallback(R.string.formatted_number, R.string.fallback)
 
-    val valueWithFallback = 32.ifNull(R.string.fallback)
-    val nullValueWithFallback = nullInt.ifNull(R.string.fallback)
+    val valueWithFallback = 32.textIfNull(R.string.fallback)
+    val nullValueWithFallback = nullInt.textIfNull(R.string.fallback)
 }
